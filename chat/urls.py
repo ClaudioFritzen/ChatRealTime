ee213211eab6f1772755185229a594d93847fff0
Claudio Fritzen <sdfritzen96@gmail.com>
2023-08-21 23:38:28 +0100

    acertamos quase tudo, mais nao esta salvando no banco

--- chat/urls.py
+++ chat/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<str:room>/', views.room, name='room'),
-    path('checkviews/', views.checkviews, name='checkviews'),
-
+    path('', views.home, name='home'),  # Página inicial
+    path('room/<str:room>/', views.room, name='room'),  # Página da sala
+    path('checkviews/', views.checkviews, name='checkviews'),  # Verificação de visualizações
 ]
-
